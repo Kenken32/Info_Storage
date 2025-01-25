b5a37232aa891540ba4bb94dbbf92294421367df
Kenken32 <kennardkennard2712@gmail.com>
2025-01-25 09:14:58 +0700

    Add files via upload

---
+++ pages/about_dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = pd.read_excel('source.xlsx')
+
+criteria1 = data['category'] == 'non food'
+criteria2 = data['store_name'] == 'Indomaret'
+criteria3 = data['price'] > 12000
+criteria4 = (data['price'] >= 12000) & (data['price'] <= 40000)
+criteria5 = (criteria1) & (criteria2) & (criteria3)
+
+print(data[criteria5].sort_values('price',ascending=True))
