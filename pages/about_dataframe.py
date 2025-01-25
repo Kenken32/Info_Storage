93459d4c06665b223c5ce79dd664137fae897728
Kenken32 <kennardkennard2712@gmail.com>
2025-01-25 09:45:11 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -6,11 +6,14 @@ unique_category = data['category'].unique()
 
 selected_category = st.multiselect("Select category",options=unique_category)
 
-criteria1 = data['category'] == 'non food'
+criteria1 = data['category'].isin(selected_category)
+
 criteria2 = data['store_name'] == 'Indomaret'
 criteria3 = data['price'] > 12000
 criteria4 = (data['price'] >= 12000) & (data['price'] <= 40000)
 criteria5 = (criteria1) & (criteria2) & (criteria3)
+
+data = data[criteria1]
 
 print(data[criteria5].sort_values('price',ascending=True))
 st.dataframe(data)
