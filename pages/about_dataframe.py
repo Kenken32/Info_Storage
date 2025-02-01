dc8647fdba73a1d8326d62ee0bf02f5fc3099603
Kenken32 <kennardkennard2712@gmail.com>
2025-02-01 09:12:14 +0700

    Update about_dataframe.py

--- pages/about_dataframe.py
+++ pages/about_dataframe.py
@@ -14,12 +14,10 @@ price_point = st.slider("Price",min_value=minimum_price,max_value=maximum_price,
 criteria1 = data['category'].isin(selected_category)
 criteria2 = data['store_name'].isin(selected_storename)
 criteria3 = data['price'] <= price_point
+
 join_criteria = (criteria1) & (criteria2) & (criteria3)
 
-
 data = data[join_criteria]
-
-print(data[criteria5].sort_values('price',ascending=True))
 
 with st.container(border=True):
   data = data[join_criteria]
